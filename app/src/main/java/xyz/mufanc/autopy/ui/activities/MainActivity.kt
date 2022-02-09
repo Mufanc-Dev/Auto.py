79eb2fbd41e154354531ffd79b301d4f0e253707
Mufanc <1714511787@qq.com>
2022-02-09 15:40:43 +0800

    临时存档

--- app/src/main/java/xyz/mufanc/autopy/ui/activities/MainActivity.kt
+++ app/src/main/java/xyz/mufanc/autopy/ui/activities/MainActivity.kt
@@ -1,16 +1,17 @@
-package xyz.mufanc.autopy
+package xyz.mufanc.autopy.ui.activities
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.navigation.NavigationView
+import xyz.mufanc.autopy.R
 import xyz.mufanc.autopy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -24,21 +25,34 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.appBarMain.toolbar)
+        binding.apply {
+            setSupportActionBar(appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri ->
+                if (uri == null) return@registerForActivityResult
+                val stream = contentResolver.openInputStream(uri)
+                stream?.close()
+            }.let { handler ->
+                appBarMain.fab.setOnClickListener { view ->
+                    handler.launch(arrayOf("text/x-python"))
+                }
+            }
         }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
+
+//        binding.appBarMain.fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//        }
+
+        val navController = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_content_main)
+            .let { (it as NavHostFragment).navController }
         val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
+            )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
---
+++ app/src/main/java/xyz/mufanc/autopy/ui/adapters/ScriptFileListAdapter.kt
@@ -0,0 +1,25 @@
+package xyz.mufanc.autopy.ui.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ScriptFileListAdapter(
+
+) : RecyclerView.Adapter<ScriptFileListAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
